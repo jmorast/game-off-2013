974a5b006d89f1fd9326b2284aa5f82a13bbd1a1
Joshua Morast <jmorast@gmail.com>
2013-12-09 17:07:57 -0500

    moved mycash to its own class file

--- source/Main.hx
+++ source/Main.hx
@@ -13,6 +13,7 @@ import flash.events.MouseEvent;
 class Main extends Sprite {
 	static var AllMoney:Array<MyCash> = new Array();
 	static var MoneyInKitty:Float=0;
+	statis var DEBUG:Bool=true;
 	public function new () {
 		super ();
 		
@@ -53,57 +54,5 @@ class Main extends Sprite {
 	}
 	
 
-}
-
-private class MyCash extends Sprite {
-        private var gfx:Sprite;
-        public var value:Float;
-        public var gameloc:String;
-        public var initX:Float;
-        public var initY:Float;
-        //public var kittyY : Float = 700;
-//        public var kittyX : Float = 250;
-        public var kittyY : Float = 600;
-        public var kittyX : Float = 250;
-	public var clicked:Bool = false;
-
-	public function new(x:Float, y:Float, amount:Float, location:String) {
-                super();
-                var img = new Bitmap(Assets.getBitmapData("assets/money-" + amount + ".png"));
-                gfx = new Sprite();
-                gfx.x = x - (this.gfx.width / 2);
-                gfx.y = y - (this.gfx.height / 2);
-                initX = x;
-                initY = y;
-                value = amount;
-		gameloc = location;	
-                gfx.addChild(img);
-                gfx.addEventListener (MouseEvent.MOUSE_DOWN, this_onMouseDown);
-                flash.Lib.current.stage.addChild(gfx);
-		if ( gameloc == "KITTY") {
-                	Actuate.tween (gfx, 1, { x:(kittyX - (gfx.width/2) + (Math.random() * 20)), y:(kittyY - (gfx.height/2) + (Math.random()*20))}).onComplete (inKitty);
-		}
-        }
-        
-	private function inKitty():Void {
-                trace('In Kitty:' + this.value);
-                gfx.addEventListener (MouseEvent.MOUSE_DOWN, this_onMouseDown);
-        }
-
-        private function this_onMouseDown(event:MouseEvent):Void {
-                trace('Mouse Down:' + this.initX + ' : ' + this.initY + ' VALUE: ' + this.value);
-		clicked=true;
-		if (gameloc == 'KITTY') {
-                	Actuate.tween (gfx, 1, { x:this.initX, y:this.initY}).onComplete (inHand);
-		}
-        }
-	private function inHand():Void {
-                trace('In Hand:' + this.value);
-                Actuate.tween (gfx, .1, { alpha:1 }).onComplete (removegfx);
-        }
-        function removegfx():Void {
-		trace('Remove ' + value);
-                flash.Lib.current.stage.removeChild(gfx);
-        }
 }
 
---
+++ source/MyCash.hx
@@ -0,0 +1,64 @@
+package ;
+
+import flash.display.Sprite;
+import motion.Actuate;
+import openfl.Assets;
+import flash.events.Event;
+import flash.text.TextField;
+import flash.text.TextFormat;
+import flash.display.Bitmap;
+
+import flash.events.MouseEvent;
+
+private class MyCash extends Sprite {
+        private var gfx:Sprite;
+        public var value:Float;
+        public var gameloc:String;
+        public var initX:Float;
+        public var initY:Float;
+        //public var kittyY : Float = 700;
+//        public var kittyX : Float = 250;
+        public var kittyY : Float = 600;
+        public var kittyX : Float = 250;
+	public var clicked:Bool = false;
+
+	public function new(x:Float, y:Float, amount:Float, location:String) {
+                super();
+                var img = new Bitmap(Assets.getBitmapData("assets/money-" + amount + ".png"));
+                gfx = new Sprite();
+                gfx.x = x - (this.gfx.width / 2);
+                gfx.y = y - (this.gfx.height / 2);
+                initX = x;
+                initY = y;
+                value = amount;
+		gameloc = location;	
+                gfx.addChild(img);
+                gfx.addEventListener (MouseEvent.MOUSE_DOWN, this_onMouseDown);
+                flash.Lib.current.stage.addChild(gfx);
+		if ( gameloc == "KITTY") {
+                	Actuate.tween (gfx, 1, { x:(kittyX - (gfx.width/2) + (Math.random() * 20)), y:(kittyY - (gfx.height/2) + (Math.random()*20))}).onComplete (inKitty);
+		}
+        }
+        
+	private function inKitty():Void {
+                trace('In Kitty:' + this.value);
+                gfx.addEventListener (MouseEvent.MOUSE_DOWN, this_onMouseDown);
+        }
+
+        private function this_onMouseDown(event:MouseEvent):Void {
+                trace('Mouse Down:' + this.initX + ' : ' + this.initY + ' VALUE: ' + this.value);
+		clicked=true;
+		if (gameloc == 'KITTY') {
+                	Actuate.tween (gfx, 1, { x:this.initX, y:this.initY}).onComplete (inHand);
+		}
+        }
+	private function inHand():Void {
+                trace('In Hand:' + this.value);
+                Actuate.tween (gfx, .1, { alpha:1 }).onComplete (removegfx);
+        }
+        function removegfx():Void {
+		trace('Remove ' + value);
+                flash.Lib.current.stage.removeChild(gfx);
+        }
+}
+
