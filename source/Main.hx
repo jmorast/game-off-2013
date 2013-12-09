591474348f5debab313cf0cf6be638e8006b6788
Joshua Morast <jmorast@gmail.com>
2013-12-09 03:00:14 -0500

    Serious cleanup of Main into one class file

--- source/Main.hx
+++ source/Main.hx
@@ -1,6 +1,7 @@
-package;
+package ;
 
 import flash.display.Sprite;
+import motion.Actuate;
 import openfl.Assets;
 import flash.events.Event;
 import flash.text.TextField;
@@ -10,62 +11,105 @@ import flash.display.Bitmap;
 import flash.events.MouseEvent;
 
 class Main extends Sprite {
-	
 	public function new () {
 		super ();
-	    //var drawervalues : Array<Float> = [.01,.05,.10,.25,1,5,10,20];
-      	//var drawerX : Array<Float> = [100,200,300,400,100,200,300,400];
-  	    //var drawerY : Array<Float> = [600,600,600,600,500,500,500,500];
 		
+	var drawervalues : Array<Float> = [.01,.05,.10,.25,1,5,10,20];
+	var drawerX : Array<Float> = [100,200,300,400,100,200,300,400];
+	var drawerY : Array<Float> = [600,600,600,600,450,450,450,450];
 		// Display some text
-		var titleText = new MyText(140, 50, 40, 0xffffff, 'Busy Barista');
+		var titleText = new MyText(140, 50, 40, 0xff00ff, 'Busy Barista');
 		trace('Debug Info: Stage Width = ' + stage.stageWidth);
 
-		//var gameHandler = new Registry();
-		//Registry.init();
-		//var gameLoop = new CashDrawer();
-		//var newkitty = new KittyManager();
-		
-		var gameLoop = new CashDrawer();
-		//Registry.init();
-		//add(Registry.Kitty);
-		//add(Registry.CashBox);
+		// keep score
+		var fun = new KittyHandler();
+                
+		// Put cash in cashbox drawer
+                for ( i in 0...drawervalues.length ) {
+	                var cashbox = new MyCash(drawerX[i],drawerY[i],drawervalues[i],"CASHDRAWER");
+//	                addCash(drawerX[i],drawerY[i],drawervalues[i]);
+                }
+
 	}
 
-	public function dosomething() {
-		trace('mike wins');
+	public static function kittyHandlerx(operation:String, value:Float) {
+		//public var kittyTotal:Float = 0;	
+		trace('operation' + operation + ' value: ' + value);
+		if (operation == 'ADD') {
+			//kittyTotal += value;
+			trace('ADD');
+		}
 	}
 
 	static function onEnterFrame() {
 		// game loop
 	}
+
 }
 
-class MyText {
-	public var textObj:TextField;
-
-	public function new(x:Int, y:Int, size:Int, color:Int, text:String) {
-		this.textObj = new TextField();
-		this.textObj.x = x;
-		this.textObj.y = y;
-		var font = Assets.getFont("assets/EHSMB.TTF");
-		this.textObj.defaultTextFormat =  new TextFormat(font.fontName, size, color);
-		this.textObj.embedFonts = true; 
-		this.textObj.text = text;
-		this.textObj.width = 640;
-		flash.Lib.current.addChild(this.textObj);
+class KittyHandler {
+	
+	public var amountInKitty:Float=0;
+	public function new() {
+		trace('KittyHandler added');
+		amountInKitty=0;
 	}
-
-	public function updateText(text:String) {
-		this.textObj.text = text;
+	public function addToKitty(value:Float) {
+		trace('add: ' + value);
+		amountInKitty += value;
 	}
+}
 
-	public function hideText() {
-		flash.Lib.current.removeChild(this.textObj);
-	}
-	public function showText() {
-		flash.Lib.current.addChild(this.textObj);
-	}
+private class MyCash extends Sprite {
+        private var gfx:Sprite;
+        public var value:Float;
+        public var gameloc:String;
+        public var initX:Float;
+        public var initY:Float;
+        public var kittyY : Float = 700;
+        public var kittyX : Float = 250;
 
+        public function new(x:Float, y:Float, amount:Float, location:String) {
+                super();
+                initX = x;
+                initY = y;
+
+                var img = new Bitmap(Assets.getBitmapData("assets/money-" + amount + ".png"));
+                gfx = new Sprite();
+                gfx.x = x - (this.gfx.width / 2);
+                gfx.y = y - (this.gfx.height / 2);
+                value = amount;
+		gameloc = location;	
+                gfx.addChild(img);
+                gfx.addEventListener (MouseEvent.MOUSE_DOWN, this_onMouseDown);
+                flash.Lib.current.stage.addChild(gfx);
+		if ( gameloc == "KITTY") {
+//                	Actuate.tween (gfx, 1, { (kittyX - (gfx.width/2) + (Math.random() * 20)), (kittyY - (gfx.height/2) + (Math.random()*20))}).onComplete (inKitty);
+                	Actuate.tween (gfx, 1, { x:200,y:200 }).onComplete (inKitty);
+		}
+        }
+        
+	private function inKitty():Void {
+                trace('In Kitty:' + this.value);
+//thisdontworkyet		fun.addToKitty(this.value);
+                gfx.addEventListener (MouseEvent.MOUSE_DOWN, this_onMouseDown);
+        }
+
+        private function this_onMouseDown(event:MouseEvent):Void {
+                //trace('Mouse Down:' + initX + ' : ' + initY + ' VALUE: ' + value);
+		if (gameloc == 'CASHDRAWER') {
+                	var tmpMove = new MyCash(initX,initY, value, "KITTY");
+		} else if (gameloc == 'KITTY') {
+                	Actuate.tween (gfx, 1, { x:initX, y:initY}).onComplete (inHand);
+		}
+        }
+	private function inHand():Void {
+                trace('In Hand:' + this.value);
+                Actuate.tween (gfx, .1, { alpha:1 }).onComplete (removegfx);
+        }
+        function removegfx():Void {
+		trace('Remove ' + value);
+                flash.Lib.current.stage.removeChild(gfx);
+        }
 }
 
---
+++ source/MyText.hx
@@ -0,0 +1,40 @@
+package ;
+
+import flash.display.Sprite;
+import openfl.Assets;
+import flash.events.Event;
+import flash.text.TextField;
+import flash.text.TextFormat;
+import flash.display.Bitmap;
+
+import flash.events.MouseEvent;
+
+class MyText {
+	public var textObj:TextField;
+
+	public function new(x:Int, y:Int, size:Int, color:Int, text:String) {
+		trace('text: ' + text);
+		this.textObj = new TextField();
+		this.textObj.x = x;
+		this.textObj.y = y;
+		var font = Assets.getFont("assets/EHSMB.TTF");
+		this.textObj.defaultTextFormat =  new TextFormat(font.fontName, size, color);
+		this.textObj.embedFonts = true; 
+		this.textObj.text = text;
+		this.textObj.width = 640;
+		flash.Lib.current.addChild(this.textObj);
+	}
+
+	public function updateText(text:String) {
+		this.textObj.text = text;
+	}
+
+	public function hideText() {
+		flash.Lib.current.removeChild(this.textObj);
+	}
+	public function showText() {
+		flash.Lib.current.addChild(this.textObj);
+	}
+
+}
+
